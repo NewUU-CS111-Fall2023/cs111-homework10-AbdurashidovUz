7ebecfdbc769cdea96e82667aeedd13ace1fb65b
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-19 17:24:02 +0500

    Update task_10.cpp

--- src/cpp/task_10.cpp
+++ src/cpp/task_10.cpp
@@ -1,9 +1,32 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <cstdlib>
 
-class Problem10 {
-public:
-};
+int main() {
+    int numVariables, numClauses;
+
+    std::cout << "Number of variables: ";
+    std::cin >> numVariables;
+
+    std::cout << "Number of clauses: ";
+    std::cin >> numClauses;
+
+    std::cout << "Generated 3-SAT formula:" << std::endl;
+
+    for (int i = 0; i < numClauses; ++i) {
+        char vars[3];
+
+        for (int j = 0; j < 3; ++j) {
+            char variable = 'A' + rand() % numVariables;
+            bool isNegated = (rand() % 2 == 0);
+            vars[j] = (isNegated) ? '~' : variable;
+        }
+
+        std::cout << "(" << vars[0] << " | " << vars[1] << " | " << vars[2] << ")";
+
+        if (i < numClauses - 1) {
+            std::cout << " & ";
+        }
+    }
+
+    return 0;
+}
