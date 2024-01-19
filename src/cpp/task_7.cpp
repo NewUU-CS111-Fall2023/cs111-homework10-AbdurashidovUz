a0ede1e61a507775da693c1f5ff0a599ce981792
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-19 17:18:51 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,22 @@
-/*
- * Author:
- * Date:
- * Name:
- */
-
-class Problem7 {
+class Solution {
 public:
+    int numRabbits(vector<int>& answers) {
+        sort(answers.begin() , answers.end());
+        int cnt = 1,len = 0, sum=0, extra=0;
+       
+        for(int i=0; i<answers.size(); i++)
+        {
+            if( i==answers.size()-1 || answers[i]!=answers[i+1]){
+                int modValue;
+                modValue = cnt%(answers[i]+1);
+               if(modValue) extra+=(answers[i]+1-modValue);
+                cnt=1;
+            }else{
+                cnt++;
+            }
+        }
+        
+        return answers.size()+extra ;
+
+    }
 };
