239d66293b5deed7e82753188a2cc7bb8fef5175
AbdurashidovUz <127374337+AbdurashidovUz@users.noreply.github.com>
2024-01-19 17:04:59 +0500

    Update task_1.cpp

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -1,9 +1,16 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+class Solution {
+ public:
+  ListNode* mergeNodes(ListNode* head) {
+    if (head == nullptr)
+      return nullptr;
+    if (!head->next->val) {
+      ListNode* node = new ListNode(head->val);
+      node->next = mergeNodes(head->next->next);
+      return node;
+    }
 
-class Problem1 {
-public:
+    ListNode* next = mergeNodes(head->next);
+    next->val += head->val;
+    return next;
+  }
 };
